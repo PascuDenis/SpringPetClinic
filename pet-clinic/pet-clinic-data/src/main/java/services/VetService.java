cc39a41156873fab51dca006399fccbd8287ec50
Denis <denis.pascu.dp98@gmail.com>
2020-04-03 00:05:36 +0300

    Added abstract map services. Added clinic index page and controller. Closes #6. Closes #8.

--- pet-clinic/pet-clinic-data/src/main/java/services/VetService.java
+++ pet-clinic/pet-clinic-data/src/main/java/services/VetService.java
@@ -4,8 +4,5 @@ import model.Vet;
 
 import java.util.Set;
 
-public interface VetService {
-    Vet findById(Long id);
-    Vet save(Vet pet);
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long> {
 }
